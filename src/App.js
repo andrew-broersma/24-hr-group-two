d8cffe44c414c21e2709072ec657c9df1eb30d32
Andrew Broersma <broersma.andrew@gmail.com>
2021-12-08 15:08:04 -0500

    actual location works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import DisplayApis from './Components/DisplayApis';
 
 function App() {
+
+  const [location, setLocation] = useState("");
+
+
+  const fetchLocation = () => {
+
+    let url = "http://ip-api.com/json/"
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => setLocation(data))
+    .catch((err) => err)
+
+  }
+  console.log(location)
+
+  useEffect(() => {
+    fetchLocation();
+  }, [])
+
   return (
     <div >
-      <DisplayApis />
-      <div></div>
+      <DisplayApis location={location}/>
     </div>
   );
 }
