f6bc1c5055580711770cd4dc7e46022998c78a2c
Andrew Broersma <broersma.andrew@gmail.com>
2021-12-08 13:42:04 -0500

    second commit

---
+++ src/Components/DisplayApis.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NASA from "./NASA";
+import Weather from "./Weather";
+import Ticketmaster from "./Ticketmaster";
+
+const DisplayApis = () => {
+    return(
+        <div>
+            <NASA />
+            <Weather />
+            <Ticketmaster />
+        </div>
+    )
+}
+
+export default DisplayApis;
