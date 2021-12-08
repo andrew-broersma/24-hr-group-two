24381a4bf6f847de62e4f2df984d01cf218172cd
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2021-12-08 12:13:10 -0800

    Nasa API  w/key started

--- src/Components/NASA.jsx
+++ src/Components/NASA.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
-const NASA = () => {
+const NASA = (props) => {
+
+    const [lat, setLat] = useState('');
+    const [lon, setLon] = useState('');
+
+    let url = `https://api.nasa.gov/planetary/earth/imagery?${lon}&${lat}&api_key=${API_KEY}`
+
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+
     return(
-        <div>
+        <div id="displayAPI">
             
         </div>
     )
