76e9e34dc9e1614d641e2ec6a54247b696c93436
Andrew Broersma <broersma.andrew@gmail.com>
2021-12-09 10:31:43 -0500

    added accordion

--- src/Components/DisplayApis.jsx
+++ src/Components/DisplayApis.jsx
@@ -2,32 +2,55 @@ import React, { useState, useEffect } from "react";
 import NASA from './NASA'
 import Weather from "./Weather";
 import Ticketmaster from "./Ticketmaster";
-import { Table } from 'reactstrap'
+import { Accordion, AccordionHeader, AccordionItem, Table } from 'reactstrap'
 
 const DisplayApis = (props) => {
 
 
     return(
         <div>
-            <NASA location={props.location} />
-            <Weather lat={props.lat} lon={props.lon} />
-            <Table
-                bordered
-                dark
-                responsive
-                striped
-            >
-                        <thead>
-                            <tr>
-                                <th>
-                                    Event
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <Ticketmaster lat={props.lat} lon={props.lon} />
-                        </tbody>
-            </Table>
+            <Accordion open="1" toggle={function noRefCheck(){}}>
+                <AccordionItem>
+                    <AccordionHeader targetId="1">
+                        NASA api
+                    </AccordionHeader>
+                    <AccordionItem accordionId="1">
+                        <NASA location={props.location} />
+                    </AccordionItem>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionHeader targetId="2">
+                        Weather api
+                    </AccordionHeader>
+                    <AccordionItem accordionId="2">
+                        <Weather lat={props.lat} lon={props.lon} />
+                    </AccordionItem>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionHeader targetId="3">
+                        TicketMaster api
+                    </AccordionHeader>
+                    <AccordionItem accordionId="3">
+                        <Table
+                            bordered
+                            responsive
+                            size="sm"
+                            striped
+                        >
+                            <thead>
+                                <tr>
+                                    <th>
+                                        Event
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <Ticketmaster lat={props.lat} lon={props.lon} />
+                            </tbody>
+                        </Table>
+                    </AccordionItem>
+                </AccordionItem>
+            </Accordion>
         </div>
     )
 }
